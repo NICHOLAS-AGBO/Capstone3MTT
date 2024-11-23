06d65a39389b0745c637363862c41e5c26186ebd
Chukwunonso10 <kuzuechinonsojude@gmai.com>
2024-11-23 15:49:40 +0100

    still fixing

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
-    cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: true,sameSite: 'lax', maxAge: 24 * 60 * 60 * 1000} // 24 hours
+    cookie: { secure: process.env.NODE_ENV === 'production', domain:'.vercel.app', httpOnly: true,sameSite: 'lax', maxAge: 24 * 60 * 60 * 1000} // 24 hours
   }));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
