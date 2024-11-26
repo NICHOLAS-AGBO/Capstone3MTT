618c5cdbfaafe1a65ab55e82dbc5abcec9c95a06
Chukwunonso10 <kuzuechinonsojude@gmai.com>
2024-11-26 06:36:45 +0100

    updates

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,20 @@
 const User = require('../models/user');
 
 const auth = async (req, res, next) => {
-  if (!req.session.userId) {
-    return res.status(401).send({ error: 'Please authenticate.' });
-  }
-
-  try {
-    const user = await User.findById(req.session.userId);
-    if (!user) {
-      throw new Error();
+    if (!req.session || !req.session.userId) {
+        return res.status(401).send({ error: 'Please authenticate.' });
     }
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(401).send({ error: 'Please authenticate.' });
-  }
+
+    try {
+        const user = await User.findById(req.session.userId);
+        if (!user) {
+            throw new Error();
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: 'Please authenticate.' });
+    }
 };
 
 module.exports = auth;
