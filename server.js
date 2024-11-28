2d31ad9f20b0963255f5c28eadce8f476e8d7141
NICHOLAS AGBO <ndcconcept@gmail.com>
2024-11-28 21:52:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors({
 //app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
-
+app.enable('trust proxy');
 // Session configuration
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -39,10 +39,10 @@ app.use(session({
   }),
   cookie: { 
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
+    // httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax'
-  }
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+  }, proxy: true
 }));
 
   app.use((req, res, next) => {
